13ac2e3f81a70be0234be5877da67e4eb2f3cb9e
Iason Sarantopoulos <iason@sarantop.com>
2021-12-01 10:10:30 +0200

    Fix imports

--- clt_core/__init__.py
+++ clt_core/__init__.py
@@ -6,14 +6,8 @@ CROP_TABLE = 193  # always square
 from clt_core.util.orientation import Quaternion
 from clt_core.util.math import min_max_scale
 from clt_core.util.cv_tools import PinholeCameraIntrinsics, PointCloud, Feature, get_circle_mask, get_circle_mask_2, rgb2gray
-from clt_core.core import MDP, Env, Robot, Camera, Transition, Agent, run_episode, train, eval, analyze_data
+import clt_core.util.pybullet as pybullet
+
+from clt_core.core import MDP, Env, Robot, Camera, Transition, Agent, run_episode, train, eval, analyze_data, Push
 from clt_core.env import UR5Bullet, BulletEnv
 from clt_core.util.info import Logger, info, warn, error, natural_keys
-from clt_core.push_primitive import Push, PushTarget
-from clt_core.algos.ddpg import DDPG
-
-# Aliases of modules
-import clt_core.mdp as mdp
-import clt_core.algos as algo
-import clt_core.algos.conv_ae as conv_ae
-import clt_core.util.pybullet as pybullet
